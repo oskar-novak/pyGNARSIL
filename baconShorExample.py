4d3744258599328ec6bdc6bd0fd972d0f677a9f7
Oskar Novak <138525379+oskar-novak@users.noreply.github.com>
2025-10-21 00:19:36 -0700

    Implement execution time measurement for pyGNARSIL
    Added timing for execution of both single and parallelized versions of pyGNARSIL function.

--- baconShorExample.py
+++ baconShorExample.py
@@ -1,3 +1,8 @@
+import time
+import numpy as np
+import itertools
+import math
+from numba import njit, prange
 Sz=np.array([[1,1,1,1,1,1,0,0,0],[0,0,0,1,1,1,1,1,1]])
 Sx=np.array([[1,1,0,1,1,0,1,1,0],[0,1,1,0,1,1,0,1,1]])
 Lx=np.array([1,0,0,1,0,0,1,0,0])
@@ -11,7 +16,26 @@ Lz=np.hstack((np.zeros(Lz.shape),Lz))
 S=np.vstack((Sx,Sz))
 baconShor=np.vstack((Lx,S,Lz))
 
+#single loop versiobn
+start_time = time.time()
 sols=pyGNARSIL(baconShor,[1,2,3,4],2,3)
-print('')
+end_time = time.time()
+elapsed_time = end_time - start_time
+
+print(f"Execution time (time.time()): {elapsed_time:.6f} seconds")
 print(sols[0])
+
+
+
+
+#parallelized version in Numba
+start_time = time.time()
+sols=pyGNARSIL_par(baconShor,[1,2,3,4],2,3)
+end_time = time.time()
+elapsed_time = end_time - start_time
+print(f"Execution time (time.time()): {elapsed_time:.6f} seconds")
+print(sols[0])
+
+#first time will be slower due to compilation!! 
+
 
